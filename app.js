b16b9508adcbce39a6a75ef9bb02bbbadea059cc
akbarimo <mo.akbari15@gmail.com>
2022-11-07 11:47:20 -0800

    Completed React Component

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// TODO
+const GroceryList = () => {
+  return (
+    <div>
+    <ul>
+      <GroceryListItems items={['Cucumber', 'Kale', 'Papaya', 'Carrot']}/>
+    </ul>
+    </div>
+  )
+}
+
+const GroceryListItems = ({items}) => {
+  return (
+    <ul>
+      {items.map((todo) => {
+        return (
+          <ToDoListItem todo ={todo}/>
+        )
+      })
+      }
+    </ul>
+  );
+}
+
+const ToDoListItem = (props) => {
+  const [isStriked, setIsStriked] = React.useState(false);
+  const [isHovered, setIsHovered] = React.useState(false);
+  const style = {
+    textDecoration: isStriked ? 'line-through' : 'none',
+    fontWeight: isHovered ? 'bold' : 'normal'
+  }
+  return (
+    <li style={style} onClick={
+      () => setIsStriked(!isStriked)
+    }
+    onMouseEnter={
+      () => setIsHovered(!isHovered)
+    } onMouseLeave={
+      () => setIsHovered(!isHovered)
+    }>
+    {props.todo}
+    </li>
+  )
+}
+
+ReactDOM.render(<GroceryList/>, document.getElementById('app'));
